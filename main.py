970addf969748cc8d8cc231c1c8f6793356cb085
Benjamin Chan <benjamin.v.chan@gmail.com>
2024-09-20 14:20:03 -0700

    Guess check logic and keep track of history

---
+++ main.py
@@ -0,0 +1,45 @@
+import random
+
+word_choices = [
+    "fluff",
+    "crate",
+    "decks",
+    "faint",
+    "bring",
+    "crash",
+    "prone"
+]
+
+def evaluate_status(correct_word_list, guess_list):
+    guess_status = []
+    for index in range(len(correct_word_list)):
+        if guess_list[index] == correct_word_list[index]:
+            guess_status.append('correct')
+        elif guess_list[index] in correct_word_list:
+            guess_status.append('contains')
+        else:
+            guess_status.append('incorrect')
+    return guess_status
+
+def main():
+
+    correct_word_list = list(random.choice(word_choices))
+    print(correct_word_list)
+    guess_history = []
+    guess_history_status = []
+    guesses = 6
+
+    while guesses > 0:
+        print()
+        
+        guess_list = list(input('Guess: ').lower())
+        guess_history.append(guess_list)
+        guess_status = evaluate_status(correct_word_list, guess_list)
+        guess_history_status.append(guess_status)
+        
+        print(f'guess_history: {guess_history}')
+        print(f'guess_history_status: {guess_history_status}')
+
+        guesses -= 1
+
+main()
